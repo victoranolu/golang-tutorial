e6f66e552dfd08384de07ada2b1f2793e59863c7
victoranolu <afactor05@gmail.com>
2025-01-02 09:58:36 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,55 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	fmt.Println("Hello Go")
+// 	var agency string = "Go Language Programming"
+// 	fmt.Println("Welcome to", agency)
+// 	name := "Victor"
+// 	fmt.Println("Hello,", name, "!")
+// 	TotalCars := 50
+// 	fmt.Println("The total numbers of cars are", TotalCars, "I am rich and wealthy")
+
+// 	startingPrice := 70.42
+
+// 	fmt.Printf("Our price starts at %v\n", startingPrice)
+// 	fmt.Println("Choose from our catalogue")
+
+// 	str := "Supercalafragilisticespialidious"
+// 	length := len(str)
+// 	fmt.Println(length)
+// }
+
+// Variables
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"strings"
+// )
+
+// func main() {
+// 	str1 := "Come outside and play"
+
+// 	wIndex := strings.Index(str1, "o")
+// 	substr := str1[wIndex:21]
+// 	fmt.Println(substr)
+// }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"strings"
+// )
+
+// func main() {
+// 	str1 := "come and be going"
+// 	str2 := "do not eat"
+
+// 	strings.EqualFold(str1, str2)
+// 	fmt.Println(strings.EqualFold(str1, str2))
+// }
+
